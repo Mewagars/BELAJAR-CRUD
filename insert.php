3cf1252560bb12715b731214facfeddb0d88f1c6
mewagars <megawagarenggaling27@gmail.com>
2022-01-26 13:46:39 +0700

    form.php

--- insert.php
+++ insert.php
@@ -1,11 +1,23 @@
 <?php
+$firsname="mewaga";
+$email="megawagarenggaling@gmail.com";
+$lastname="mewagarenggaling";
+if (!empty($_POST)){
+    $firstname=$_POST ["firstname"];
+    $email=$_POST ["email"];
+    $lastname=$_POST ["lastname"];
+} else {
+    die("input harus lewat form");
+}
 include "config.php";
-$sql = "INSERT INTO Myguest (firstname, lastname, email)
-VALUES ('john','doe','john@example.com')";
-if (mysqli_query($conn,$sql)) {
+$sql = "INSERT INTO MyGuest (firstname, lastname, email)
+VALUES (?,?,?)";
+$stmt = mysqli_prepare($conn,$sql);
+mysqli_stmt_bind_param($stmt, "sss" , $firstname,$lastname,$email);
+if (mysqli_stmt_execute($stmt)) {
     echo "New record created successfully";
 } else {
-    echo "Error:".$sql."<br>".mysqli_error($conn);
+    echo "Error:".$sql ."<br>". mysqli_error($conn);
 }
 mysqli_close($conn);
 ?>
