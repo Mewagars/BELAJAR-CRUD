63d90839475057ed2d77a30fd3933a95ac94c0b6
zvyr <90256140+zvyr@users.noreply.github.com>
2022-02-26 09:25:20 +0700

    Update config.php

--- config.php
+++ config.php
@@ -3,9 +3,9 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "myDB";
-// create connection 
+// membuat koneksi 
 $conn = mysqli_connect($servername,$username,$password,$dbname);
-// check connection
+// mengecek koneksi
 if (!$conn) {
     die("connection failed:".mysqli_connect_error());
 }
